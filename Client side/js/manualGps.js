4d237819a0d271439d114e69921bcdea9eb22a1c
Lihay Morad <44345425+LihayMorad@users.noreply.github.com>
2018-10-27 18:02:41 +0300

    Deleted unnecessary code and comments.

--- Client side/js/manualGps.js
+++ Client side/js/manualGps.js
@@ -1,29 +1,21 @@
-///// Google Map
 var map, MyLocationinfoWindow;
 var Lat , Lng;
 
 function initMap() {
 
-    // Maps initialization options (default location, zoom, etc..)
     var options = {
         zoom: 6,
-        // minZoom: 10,
-        // maxZoom: 20,
-        //disableDoubleClickZoom: true,
         center: {
             lat: 32.109333,
             lng: 34.855499
         }
     }
-
     
     // Map initialize with the options above
     map = new google.maps.Map(document.getElementById('map'), options);
     var gmarkers = [];
-    
-
+  
     google.maps.event.addListener(map, 'click', function(event) {
-        //alert( 'Lat: ' + event.latLng.lat() + ' and Longitude is: ' + event.latLng.lng() );
         Lat = event.latLng.lat();
         Lng = event.latLng.lng();
         for(i=0; i<gmarkers.length; i++){
@@ -35,6 +27,5 @@ function initMap() {
           });
           gmarkers.push(marker);
      });
-
     
 }
